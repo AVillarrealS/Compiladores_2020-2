e509cb15f8f9a78d52c67846e69b6a5db9185cb5
J Antonio Villarreal Silva <antonio.fi.unam@gmail.com>
2020-04-15 15:36:18 -0500

    Prueba final

--- CódigoPrueba/prueba.c
+++ CódigoPrueba/prueba.c
@@ -26,7 +26,7 @@ int main (int argc, char** argv){
     printf("\t¡Gracias!\n");
 
     printf("\n\tAgregamos un nuevo commit");
-    printf("\n\tPrueba 2.");
+    printf("\n\tPrueba 2.\n");
 
     return 0;
 }
