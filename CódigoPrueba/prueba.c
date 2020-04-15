d7c4f505652b669fffe5ce1061db7f401df3fce4
J Antonio Villarreal Silva <antonio.fi.unam@gmail.com>
2020-04-15 15:20:16 -0500

    Agrego archivos pruebas

---
+++ CódigoPrueba/prueba.c
@@ -0,0 +1,29 @@
+//Código para probar nuevo repositorio en GitHUb
+//Clase de Compiladores 2020-2
+//Grupo 02
+//Profesor: Ing. Adrián Ulises Mercado Martínez
+//ALumno: Villarreal Silva José Antonio
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char** argv){
+    printf("\tHola Mundo\n\n");
+    int a=9;
+    int b=5;
+
+    int c = a+b;
+    printf("\t\tResultado de 9+5= %d\n",c);
+    //int c=0;
+
+    int d= a*b;
+    printf("\t\tResultado de 9*5= %d\n",d);
+    //int c=0;
+
+    int e = b-a;
+    printf("\t\tResultado de 5-9= %d\n\n",e);
+
+    printf("\t¡Gracias!\n");
+
+    return 0;
+}
